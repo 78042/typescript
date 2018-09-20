80a7193b7e85e4487166de1847934c11d82a7bb1
78042 <xl78042@163.com>
2018-09-21 00:10:33 +0800

    泛型->命名空间

--- demo.ts
+++ demo.ts
@@ -122,7 +122,7 @@ function createSquare(config: SquareConfig): { color: string; area: number } {
 //colour不是接口定义的，在ts中会报错
 //解决方法1：使用类型断言绕开这些检查
 let mySquare = createSquare({ colour: "red", width: 100 } as SquareConfig);
-//解决方法2(最佳的方式):添加一个字符串索引签名
+//解决方法2(最佳的方式(也可以用泛型解决)):添加一个字符串索引签名
 interface SquareConfig_ {
     color?: string;
     width?: number;
@@ -203,3 +203,75 @@ let deck: Deck = {
 let cardPicker = deck.createCardPicker();
 let pickedCard = cardPicker();
 //alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+
+//泛型：使返回值的类型与传入参数的类型是相同的一个函数
+function fx1<T> (arg: T):T {
+	return arg;
+}
+function fx2<T> (arg: T[]): T[] {
+	return arg;
+}
+let full_fx1: <T>(arg: T) => T = fx1;
+let full_fx2: <U>(arg: U) => U = fx1;//名称对应就可以了
+
+let full_fx3: {<T>(arg: T): T} = fx1;//带有调用签名的对象字面量来定义泛型函数
+//1
+interface for_fx {
+	<T>(arg: T): T;
+}
+let fx_4: for_fx = fx1;
+//2
+interface for_fx1<T> {
+	<T>(arg: T): T;
+}
+let fx_5: for_fx1<number> = fx1;
+
+
+let ret = fx1<string>('asd')
+//let ret = fx('asd') ->可以不加<>，因为编译器会自动识别
+
+
+//泛型类
+class GenericNumber<T> {
+    zeroValue: T;
+    add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function(x, y) { return x + y; };
+
+//场景：参数必须包含某属性
+interface If {
+	num: number
+}
+function fx_<T extends If>(arg: T): T {
+	console.log(arg.num)
+	return arg;
+}
+//fx_('asd') ->报错
+fx_({num:123,ad: 123})
+
+//枚举
+enum Direction {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+//Up使用初始化为 1。 其余的成员会从 1开始自动增长,Up不赋值时，默认为0
+
+
+//----------------------额外补充es6
+// for..in可以操作任何对象；它提供了查看对象属性的一种方法。 但是 for..of关注于迭代对象的值
+// let pets = new Set(["Cat", "Dog", "Hamster"]);
+// pets["species"] = "mammals";
+
+// for (let pet in pets) {
+//     console.log(pet); // "species"
+// }
+
+// for (let pet of pets) {
+//     console.log(pet); // "Cat", "Dog", "Hamster"
+// }
